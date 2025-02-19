e4779223ea1d343c6568736ca6c75830827d9c4d
henok-ett <kindehenok@gmail.com>
2025-02-19 14:58:38 +0300

    Update pytongame.py

--- pytongame.py
+++ pytongame.py
@@ -1,6 +1,29 @@
 import random
 import time
 
+import random
+import time
+
+# Initial Steps to invite in the game:
+print("\nWelcome to Hangman game by IT SOURCECODE\n")
+name = input("Enter your name: ")
+print("Hello " + name + "! Best of Luck!")
+time.sleep(2)
+print("The game is about to start!\n Let's play Hangman!")
+time.sleep(3)
+
+
+# The parameters we require to execute the game:
+def main():
+    global count
+    global display
+    global word
+    global already_guessed
+    global length
+    global play_game
+    words_to_guess = ["january","border","image","film","promise","kids","lungs","doll","rhyme","damage"
+                   ,"plants"]
+
 # Initial Steps to invite in the game:
 print("\nWelcome to Hangman game by IT SOURCECODE\n")
 name = input("Enter your name: ")
