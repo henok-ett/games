b712259f43bedaffe868074dcc4e1c10ee14d633
henok-ett <kindehenok@gmail.com>
2025-02-20 15:49:22 +0300

    Update pytongame.py

--- pytongame.py
+++ pytongame.py
@@ -1,6 +1,17 @@
 import random
 import time
 
+import random
+import time
+
+# Initial Steps to invite in the game:
+print("\nWelcome to Hangman game by IT SOURCECODE\n")
+name = input("Enter your name: ")
+print("Hello " + name + "! Best of Luck!")
+time.sleep(2)
+print("The game is about to start!\n Let's play Hangman!")
+time.sleep(3)
+
 # Initial Steps to invite in the game:
 print("\nWelcome to Hangman game by IT SOURCECODE\n")
 name = input("Enter your name: ")
